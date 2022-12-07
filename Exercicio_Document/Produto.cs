3417f7654b97aa1c3ffc82945c2bb223bc483231
ByMaluf <brenno.maluf@gmail.com>
2022-12-07 17:17:17 -0400

    Adicionar arquivos de projeto.

---
+++ Exercicio_Document/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Document
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public int Qtd { get; set; }
+        public double Valor { get; set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double valor, int qtd)
+        {
+            Nome = nome;
+            Valor = valor;
+            Qtd = qtd;  
+        }
+
+
+        public double Totalizar()
+        {
+            return Valor * Qtd;
+        }
+    }
+}
---
+++ Exercicio_Document/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Globalization;
+
+namespace Exercicio_Document
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            // ---------------- 1º EXERCÍCIO 
+
+            /*
+             
+            Console.WriteLine("Informe o nome do arquivo ou sua extenção: ");
+            try
+            {
+
+                Console.WriteLine("Infome o nome ou o tipo de arquivo que você está procurando");
+                string nomeArquivoTipo = Console.ReadLine();
+
+                IEnumerable<string> arquivos = Directory.EnumerateFiles(@"c:\tmp", "*" + nomeArquivoTipo + "*", SearchOption.AllDirectories);
+                Console.WriteLine("\n\rArquivos\n\r");
+                foreach (var item in arquivos)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\rOcorreu um erro\n\r" + e.Message);
+            }
+
+            */
+
+            Console.WriteLine("------------------------------------------------");
+
+
+            // ---------------- 1º EXERCÍCIO
+
+            /*
+            Console.WriteLine("Informe o nome do arquivo ou sua extenção: ");
+            Console.WriteLine();
+
+            try
+            {
+                string nomeArquivo = Console.ReadLine();
+                Console.WriteLine();
+                var arquivos = from arquivo in Directory.EnumerateFiles(@"c:\tmp", "*.*")
+                               where arquivo.ToLower().Contains(nomeArquivo)
+                               select arquivo;
+                Console.WriteLine();
+
+                foreach (var item in arquivos)
+                {
+                    Console.WriteLine("{0}", item);
+                }
+                Console.WriteLine();
+                Console.WriteLine("{0} Arquivos encontrados.", arquivos.Count<string>().ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um erro! " + e.Message);
+            }
+             */
+
+            Console.WriteLine("------------------------------------------------");
+
+            // ---------------- 2º EXERCÍCIO         
+            try
+            {
+                //Criação da pasta
+                Directory.CreateDirectory(@"c:\tmp\csv");
+
+                string[] prod = new string[8];
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Informações do produto: (Nome) (Preço) (Quantidade):");
+                    prod[i] = Console.ReadLine();
+                }
+                
+                //Criação do arquivo ItensVendidos.csv
+                //Gravação das informações dos produtos dentro do arquivo ItensVendidos.csv
+                using (StreamWriter escritorArquivo = File.AppendText(@"c:\tmp\csv\ItensVendidos.csv"))
+                {
+                    foreach (var item in prod)
+                    {
+                        string[] campos = item.Split(',');
+                        string nome = campos[0];
+                        double preco = double.Parse(campos[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(campos[2]);
+
+                        Produto produtos = new Produto(nome, preco, quantidade);
+
+                        escritorArquivo.WriteLine(produtos.Nome + ", " + produtos.Valor.ToString("f2", CultureInfo.InvariantCulture) + ", " + produtos.Qtd);
+                    }
+                }
+
+
+
+
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu um erro!\n\r" + e.Message);
+            }
+
+            //escritorArquivo.WriteLine(produtos.Nome + ", " + produtos.Totalizar().ToString("F2"), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("------------------------------------------------");
+
+            // ---------------- 3º EXERCÍCIO
+
+        }
+    }
+}
